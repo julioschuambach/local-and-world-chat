dd8c0b9db9211e8df10495e5c2d7ebe3769ec6dd
julioschuambach <julioschuambach.dev@gmail.com>
2023-12-13 21:03:51 -0300

    feat: implemented sender and multiple listener channels

--- MultiChannel/Sender/Program.cs
+++ MultiChannel/Sender/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace Sender
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Sender
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Sender started!");
+            Console.WriteLine();
+
+            var server = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "user",
+                Password = "Pass@123"
+            };
+
+            var connection = server.CreateConnection();
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Chat",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    while (true)
+                    {
+                        Console.Write("Message: ");
+
+                        string message = Console.ReadLine();
+
+                        if (message == "/exit")
+                            break;
+
+                        var messageBody = Encoding.UTF8.GetBytes(message);
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        channel.BasicPublish(
+                            exchange: "",
+                            routingKey: "Chat",
+                            basicProperties: properties,
+                            body: messageBody);
+
+                        Console.WriteLine("[x] Sent message: {0}", message);
+                    }
+                }
+
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
--- MultiChannel/WorldChatReceiver/Program.cs
+++ MultiChannel/WorldChatReceiver/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace WorldChatReceiver
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace WorldChatReceiver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("World chat started!");
+            Console.WriteLine();
+
+            var server = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "user",
+                Password = "Pass@123"
+            };
+
+            var connection = server.CreateConnection();
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Chat",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    channel.BasicQos(
+                        prefetchSize: 0,
+                        prefetchCount: 1,
+                        global: false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("[x] Received message: {0}", message);
+
+                        channel.BasicAck(
+                            deliveryTag: ea.DeliveryTag,
+                            multiple: false);
+                    };
+
+                    channel.BasicConsume(
+                        queue: "Chat",
+                        autoAck: false,
+                        consumer: consumer);
+
+                    Console.WriteLine("Press enter to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
